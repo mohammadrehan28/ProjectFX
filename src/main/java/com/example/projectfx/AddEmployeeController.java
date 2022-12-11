38b6493a7110ce7f664f06134b95c6610dec5ce7
MohammadRehan <mohammadre1654@gmail.com>
2022-12-11 22:32:32 +0200

    Night

--- src/main/java/com/example/projectfx/AddEmployeeController.java
+++ src/main/java/com/example/projectfx/AddEmployeeController.java
@@ -1,4 +1,80 @@
 package com.example.projectfx;
 
-public class AddEmployeeController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddEmployeeController implements Initializable {
+
+    @FXML
+    private Label AddEmployee;
+
+    @FXML
+    private HBox Manager;
+
+    @FXML
+    private ComboBox<?> comboType;
+
+    @FXML
+    private DatePicker textBirth;
+
+    @FXML
+    private TextField textCity;
+
+    @FXML
+    private TextField textDegree;
+
+    @FXML
+    private TextField textDegreeManager;
+
+    @FXML
+    private TextField textDep;
+
+    @FXML
+    private TextField textDriv;
+
+    @FXML
+    private TextField textExp;
+
+    @FXML
+    private TextField textFirst;
+
+    @FXML
+    private TextField textGender;
+
+    @FXML
+    private TextField textLast;
+
+    @FXML
+    private TextField textPhone;
+
+    @FXML
+    private TextField textQual;
+
+    @FXML
+    private TextField textSSN;
+
+    @FXML
+    private TextField textSecond;
+
+    @FXML
+    private TextField textStreet;
+
+    @FXML
+    void AddEmployeeListener(MouseEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
