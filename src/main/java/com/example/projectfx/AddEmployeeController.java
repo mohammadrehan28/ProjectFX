7b64934654c59bd1f3f188b19926c73d3e29899f
MohammadRehan <mohammadre1654@gmail.com>
2022-12-12 10:34:26 +0200

    Morning

--- src/main/java/com/example/projectfx/AddEmployeeController.java
+++ src/main/java/com/example/projectfx/AddEmployeeController.java
@@ -9,8 +9,15 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import oracle.jdbc.pool.OracleDataSource;
 
+import javax.swing.*;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 public class AddEmployeeController implements Initializable {
@@ -37,7 +44,7 @@ public class AddEmployeeController implements Initializable {
     private VBox VBoxType;
 
     @FXML
-    private ComboBox<?> comboType;
+    private ComboBox<String> comboType;
 
     @FXML
     private DatePicker textBirth;
@@ -86,11 +93,64 @@ public class AddEmployeeController implements Initializable {
 
     @FXML
     void AddEmployeeListener(MouseEvent event) {
+        try {
+            OracleDataSource ods = new OracleDataSource();
+            ods.setURL("jdbc:oracle:thin:@localhost:1521:xe");
+            ods.setUser("mohammad");
+            ods.setPassword("123456");
+            Connection con = ods.getConnection();
+            String all = "select SSN from Employee";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(all);
+            while (rs.next()) {
+                String SSN = rs.getString(1);
+                if(SSN.equals(textSSN.getText())) {
+                    JOptionPane.showMessageDialog(null, "The SSN is already contains", "ERROR", JOptionPane.ERROR_MESSAGE);
+                    con.close();
+                    return;
+                }
+            }
+            String SSN = textSSN.getText();
+            LocalDate Birth = textBirth.getValue();
+            //DATE '1998-12-17'
+            String FormatBirth = "DATE '" + Birth.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")) +"'";
+            String Phone = textPhone.getText();
+            String First = textFirst.getText();
+            String Second = textSecond.getText();
+            String Last = textLast.getText();
+            String Gender = textGender.getText();
+            if(Gender.toLowerCase().equals("male")) Gender = "M";
+            else if (Gender.toLowerCase().equals("female")) Gender = "F";
+            else {
+                JOptionPane.showMessageDialog(null, "The Gender is not allowed", "ERROR", JOptionPane.ERROR_MESSAGE);
+                con.close();
+                return;
+            }
+            String City = textCity.getText();
+            String Street = textStreet.getText();
+            if(comboType.getSelectionModel().getSelectedItem().equals("Manager Employee")) {
+                String Degree = textDegree.getText();
+                String Qual = textQual.getText();
+                all = "INSERT INTO Employee values("+SSN+",)";
+            }
+            else if (comboType.getSelectionModel().getSelectedItem().equals("Driver Employee")) {
 
+            }
+            else if (comboType.getSelectionModel().getSelectedItem().equals("Nursery Employee")) {
+
+            }
+            else {
+
+            }
+            con.commit();
+            con.close();
+        } catch(Exception e) {
+            System.out.println(e);
+        }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        comboType.getItems().addAll("Manager Employee", "Driver Employee", "Nursery Employee", "Project Employee");
     }
 }
